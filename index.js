030139271ccbd61db62e51dfa86c2dc1cf8a2fef
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-10-02 11:34:22 -0400

    Get issues

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function getIssues() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-students/javascript-fetch-lab-react-000'
   fetch(`https://api.github.com/repos/${repo}/issues`, {
     headers: {
       Authorization: `token ${token}`
     }
-  }).then(res => res.json()).then(()=>debugger)
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
-function showIssues(json) {
+function showIssues(issues) {
+  let source = $("#issues-template").html();
+  let template = handlebars.coplipe(source);
+  debugger;
 }
 
 function createIssue() {
@@ -31,3 +34,5 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+getIssues();
